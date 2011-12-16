dcfdc831c0ad723e0c417bdddbf673b4a6f438fb
edoules <ed.yt.ma@gmail.com>
2011-12-16 16:31:02 -0500

    Placeholder index.php
    Creating initial file to ensure all team members can get this
    repository.

---
+++ index.php
@@ -0,0 +1,12 @@
+<h1>Placeholder</h1>
+<p>"If you see this page, then you're doing it right!"</p>
+
+<p style="text-align:center;">
+<?php
+    
+    print date("Y-m-d H:i:s");
+
+?>
+</p>
+
+<p style="text-align:right;">-- Eddie</p>
